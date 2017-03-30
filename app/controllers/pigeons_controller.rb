3479745ba4f6a5159e1ffd1e7992f5ca4b2471f9
epang1 <epang1@gmail.com>
2017-03-29 21:19:26 -0400

    done

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,2 +1,57 @@
-# create your PigeonsController here
-# it should inherit from ApplicationController
+class PigeonsController < ApplicationController
+
+  get '/pigeons' do
+    erb :'pigeons/index'
+  end
+
+  post '/pigeons' do
+    new_pigeon = Pigeon.create(params)
+    new_pigeon.save
+    redirect '/pigeons'
+  end
+
+  get '/pigeons/new' do
+    erb :"pigeons/new"
+  end
+
+  post '/pigeons/new' do
+    new_pigeon = Pigeon.create(params[:pigeon])
+    new_pigeon.save
+  end
+
+  delete '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.destroy
+    redirect '/pigeons'
+  end
+
+  patch '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.name = params[:name]
+    @pigeon.gender = params[:gender]
+    @pigeon.color = params[:color]
+    @pigeon.lives = params[:lives]
+    @pigeon.save
+    redirect "/pigeons/#{params[:id]}"
+  end
+
+
+  get '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'pigeons/show'
+  end
+
+  get '/pigeons/:id/edit' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'pigeons/edit'
+  end
+
+  post '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.update(params[:pigeon])
+    @pigeon.save
+  end
+
+
+
+end
--- config.ru
+++ config.ru
@@ -4,5 +4,7 @@ if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending run `rake db:migrate` to resolve the issue.'
 end
 
+
+use Rack::MethodOverride
 use PigeonsController
 run ApplicationController
